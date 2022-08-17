eef0c3238cdc42bcfba5fae42b0a22a5cff45252
kimkh <kim-vk@mail.ru>
2022-08-17 17:56:25 +0300

    First version  only for TextBox TextBoxTests.java

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -1,32 +1,27 @@
 package com.demoqa;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 public class TextBoxTests {
 
-    @BeforeAll
-    static void configure() {
-        System.out.println("Test Inited!");
-    }
     @Test
     void fillFormTest(){
+        String MyPermanentAddress = "My permanent address";
 
         open("https://demoqa.com/text-box");
-        /* $("#userName").setValue("Khen Gi");
+        $("#userName").setValue("Khen Gi");
         $("#userEmail").setValue("kim-vk@mail.ru");
-        $("input#currentAddress").setValue("My adress");
-        $("#permanentAddress").setValue(permanentAddress);
+        $("#currentAddress").setValue("My adress");
+        $("#permanentAddress").setValue(MyPermanentAddress);
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Khen Gi"));
-        $("#output #email").shouldHave(text("kim-vk@mail.ru"));
-        $("#output p#currentAddress").shouldHave(text("My adress"));
-        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+        $("#output").shouldHave(text("Khen"));
+        $("#output").shouldHave(text("kim-vk@mail.ru"));
+        $("#output").shouldHave(text("My adress"));
+        $("#output").shouldHave(text(MyPermanentAddress));
 
-         */
     }
 
 }
